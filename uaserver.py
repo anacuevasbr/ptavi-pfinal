4aaee4fead9586d0650e47d72be95b4ae76b6d95
Ana Cuevas Bravo <acuevas@gamma10.aulas.gsyc.urjc.es>
2017-12-13 10:47:32 +0100

    empieza el servidor

---
+++ uaserver.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import socketserver
+import sys
+import uaclient
+
+class EchoHandler(socketserver.DatagramRequestHandler):
+    """
+    Echo server class
+    """
+
+    def handle(self):
+        # Escribe dirección y puerto del cliente (de tupla client_address)
+        self.wfile.write(b"Hemos recibido tu peticion")
+        while 1:
+            # Leyendo línea a línea lo que nos envía el cliente
+            line = self.rfile.read()
+            print("El cliente nos manda " + line.decode('utf-8'))
+
+            # Si no hay más líneas salimos del bucle infinito
+            if not line:
+                break
+
+if __name__ == "__main__":
+    # Creamos servidor de eco y escuchamos
+    if len(sys.argv) != 2:
+        sys.exit('Usage: python3 uaserver.py config')
+
+    datos = uaclient.parsercreator(sys.argv[1])
+    print(datos)
+    IP = datos[1]['ip']
+    if IP == '':
+        IP = '127.0.0.1'
+    PORT = int(datos[1]['puerto'])
+    serv = socketserver.UDPServer((IP, PORT), EchoHandler)
+    print("Lanzando servidor UDP de eco...")
+    serv.serve_forever()
