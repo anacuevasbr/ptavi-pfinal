4fe96044b1614330d889f781ef95d1b99b23cad2
Ana Cuevas Bravo <acuevas@alpha24.aulas.gsyc.urjc.es>
2018-01-11 17:13:44 +0100

    arreglos del log y pep8

--- uaserver.py
+++ uaserver.py
@@ -12,18 +12,23 @@ def AddtoLog(path, message, Action):
 
     f = open(path, "a")
     if Action == 'Receive':
-        Message = time.strftime('%Y%m%d%H%M%S',time.gmtime(time.time()))+ ' Received from ' + message.replace('\r\n', ' ') + '\r\n'
+        Message = time.strftime('%Y%m%d%H%M%S', time.gmtime(time.time()))
+        Message += ' Received from ' + message.replace('\r\n', ' ') + '\r\n'
     elif Action == 'Send':
-        Message = time.strftime('%Y%m%d%H%M%S',time.gmtime(time.time())) + ' Sent to ' + message.replace('\r\n', ' ') + '\r\n'
+        Message = time.strftime('%Y%m%d%H%M%S', time.gmtime(time.time()))
+        Message += ' Sent to ' + message.replace('\r\n', ' ') + '\r\n'
     elif Action == 'Error':
-        Message = time.strftime('%Y%m%d%H%M%S',time.gmtime(time.time())) + ' Error ' + message.replace('\r\n', ' ') + '\r\n'
+        Message = time.strftime('%Y%m%d%H%M%S', time.gmtime(time.time()))
+        Message += ' Error ' + message.replace('\r\n', ' ') + '\r\n'
     elif Action == 'Status':
-        Message = time.strftime('%Y%m%d%H%M%S',time.gmtime(time.time())) + message + '\r\n'
-    f.write(Message) 
+        Message = time.strftime('%Y%m%d%H%M%S', time.gmtime(time.time()))
+        Message += message + '\r\n'
+    f.write(Message)
     f.close()
 
+
 def Valid(message):
-    
+
     VAL = False
     print(message.split(' ')[0])
     if message.split(' ')[0] == 'INVITE':
@@ -46,7 +51,8 @@ def Valid(message):
             VAL = True
 
     return VAL
-    
+
+
 class EchoHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
@@ -54,9 +60,8 @@ class EchoHandler(socketserver.DatagramRequestHandler):
     RTPDATA = {}
 
     def SafeRTPData(self, DATA):
-        
-        IP = DATA[4].split(' ')[1]
-        self.RTPDATA['1'] = [IP[0:-1],DATA[7].split(' ')[1]]
+        IP = DATA[4].split(' ')[1].split('\r')[0]
+        self.RTPDATA['1'] = [IP, DATA[7].split(' ')[1].split('\r')[0]]
 
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
@@ -68,42 +73,48 @@ class EchoHandler(socketserver.DatagramRequestHandler):
             DATA.append(line.decode('utf-8'))
 
         if Valid(''.join(DATA)):
+            Client = self.client_address[0] + ':'
+            Client += str(self.client_address[1]) + ' '
 
             if DATA[0].split(' ')[0] == 'INVITE':
                 self.SafeRTPData(DATA)
                 print('Respondiendo a invite')
                 Message = ' '.join(DATA)
-                Client = self.client_address[0] + ':' + str(self.client_address[1]) + ' '
-                AddtoLog(datos[4]['path'], Client + Message, 'Send')                
+                AddtoLog(datos[4]['path'], Client + Message, 'Send')
                 self.wfile.write(b"SIP/2.0 100 Trying\r\n\r\n")
                 Message = 'SIP/2.0 100 Trying\r\n'
                 AddtoLog(datos[4]['path'], Client + Message, 'Send')
                 self.wfile.write(b"SIP/2.0 180 Ringing\r\n\r\n")
                 Message = 'SIP/2.0 180 Ringing\r\n'
                 AddtoLog(datos[4]['path'], Client + Message, 'Send')
-                
-                Message = 'SIP/2.0 200 OK\r\n' + 'Content-Type: application/sdp\r\n\r\n' + 'v=0\r\n' + 'o=' + USER + ' 127.0.0.1\r\n' + 's=hungry\r\n' + 't=0\r\n' + 'm=audio ' + datos[2]['puerto'] + ' RTP\r\n'
+
+                Message = 'SIP/2.0 200 OK\r\n'
+                Message += 'Content-Type: application/sdp\r\n\r\n'
+                Message += 'v=0\r\n' + 'o=' + USER + ' 127.0.0.1\r\n'
+                Message += 's=hungry\r\n' + 't=0\r\n' + 'm=audio '
+                Message += datos[2]['puerto'] + ' RTP\r\n'
                 self.wfile.write(bytes(Message, 'utf-8'))
                 AddtoLog(datos[4]['path'], Client + Message, 'Send')
-                
+
             elif DATA[0].split(' ')[0] == 'ACK':
                 print('Recibido ACK')
                 Message = ' '.join(DATA)
-                Client = self.client_address[0] + ':' + str(self.client_address[1]) + ' '
                 AddtoLog(datos[4]['path'], Client + Message, 'Receive')
                 audio = datos[5]['path']
-                order = "./mp32rtp -i " + self.RTPDATA['1'][0] + " -p " + self.RTPDATA['1'][1] + " < " + audio
+                order = "./mp32rtp -i " + self.RTPDATA['1'][0]
+                order += " -p " + self.RTPDATA['1'][1] + " < " + audio
                 os.system(order)
-                AddtoLog(datos[4]['path'], 'Enviando RTP', 'Send')
+                print(self.RTPDATA['1'][0])
+                Client = self.RTPDATA['1'][0] + ':'
+                Client += self.RTPDATA['1'][1] + ' '
+                AddtoLog(datos[4]['path'], Client + 'Enviando RTP', 'Send')
             elif DATA[0].split(' ')[0] == 'BYE':
                 print('Recibido bye')
                 Message = ' '.join(DATA)
-                Client = self.client_address[0] + ':' + str(self.client_address[1]) + ' '
                 AddtoLog(datos[4]['path'], Client + Message, 'Receive')
 
                 self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
                 Message = 'Sent SIP/2.0 200 OK\r\n'
-                Client = self.client_address[0] + ':' + str(self.client_address[1]) + ' '
                 AddtoLog(datos[4]['path'], Client + Message, 'Send')
             else:
                 self.wfile.write(b"SIP/2.0 405 Method Not Allowed\r\n\r\n")
@@ -113,7 +124,7 @@ class EchoHandler(socketserver.DatagramRequestHandler):
             self.wfile.write(b'SIP/2.0 400 Bad Request\r\n\r\n')
             Message = "SIP/2.0 400 Bad Request"
             AddtoLog(datos[4]['path'], Message, 'Error')
- 
+
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
     if len(sys.argv) != 2:
@@ -130,4 +141,4 @@ if __name__ == "__main__":
         serv.serve_forever()
     except KeyboardInterrupt:
         print('Cerrando servidor')
-        AddtoLog(datos[4]['path'], 'Finshing.', 'Status')        
+        AddtoLog(datos[4]['path'], 'Finshing.', 'Status')
